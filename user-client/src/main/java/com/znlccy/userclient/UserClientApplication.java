98e5c0bae9cd4267e73ae4b7c0ea37e2f2af4876
znlccy <756311868@qq.com>
2018-12-04 14:52:31 +0800

    Initial commit

---
+++ user-client/src/main/java/com/znlccy/userclient/UserClientApplication.java
@@ -0,0 +1,12 @@
+package com.znlccy.userclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserClientApplication.class, args);
+    }
+}
---
+++ user-common/src/main/java/com/znlccy/usercommon/UserCommonApplication.java
@@ -0,0 +1,12 @@
+package com.znlccy.usercommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCommonApplication.class, args);
+    }
+}
